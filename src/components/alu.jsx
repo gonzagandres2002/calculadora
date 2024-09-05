0c9db84dc3fc0c4d486c12c46d3c7675506f2b43
JuanLopez5 <juan.lopez81@udea.edu.co>
2024-09-05 15:33:46 -0500

    cambio de tamaño y colores

--- src/components/alu.jsx
+++ src/components/alu.jsx
@@ -1,15 +1,15 @@
 export default function alu() {
   return (
 
-        <div className="p-4 border-2 border-green-300 rounded-md">
+        <div className="p-4 border-blue-500 bg-blue-300/45 bg- rounded-md  w-96 h-52">
           <h2 className="font-bold mb-2">Unidad aritmético-lógica (ALU)</h2>
           <div className="flex flex-col gap-2">
-            <div className="border border-gray-300 p-2 flex justify-between">
-              <span>Acumulador</span>
+            <div className="border-2 border-gray-300 p-2 flex justify-between">
+              <span className="font-bold">Acumulador</span>
               <span>00000000</span>
             </div>
-            <div className="border border-gray-300 p-2 flex justify-between">
-              <span>Restrepo. Entrada</span>
+            <div className="border-2 border-gray-300 p-2 flex justify-between">
+              <span className="font-bold">R. Entrada</span>
               <span>00000000</span>
             </div>
           </div>
--- src/components/controlUnit.jsx
+++ src/components/controlUnit.jsx
@@ -1,17 +1,17 @@
 export default function controlUnit() {
   return (
-    <div className="p-4 border-2 border-blue-300 rounded-md">
+    <div className="p-4 bg-yellow-200 border-yellow-300 rounded-md w-96 h-52">
       <h2 className="font-bold mb-2">Unidad de control</h2>
-      <div className="border border-gray-300 p-2 mb-2">
+      <div className="border-2 border-gray-300 font-bold p-2 mb-2">
         <span>Decodificador</span>
       </div>
       <div className="flex flex-col gap-2">
-        <div className="border border-gray-300 p-2 flex justify-between">
-          <span>Cont. Programa</span>
+        <div className=" border-2 border-gray-300 p-2 flex justify-between">
+          <span className="font-bold">Cont. Programa</span>
           <span>0000</span>
         </div>
-        <div className="border border-gray-300 p-2 flex justify-between">
-          <span>R. Instrucciones</span>
+        <div className="border-2 border-gray-300 p-2 flex justify-between">
+          <span className="font-bold">R. Instrucciones</span>
           <span>00000000</span>
         </div>
       </div>
